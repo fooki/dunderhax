3b6425fcc313564ce0b093f05ed1fc5da9086b3a
Karl Johansson <karl.johansson@hemnet.se>
2022-01-13 21:17:09 +0100

    Start using typescript a lil bit

--- app/javascript/components/Main.tsx
+++ app/javascript/components/Main.tsx
@@ -2,14 +2,11 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import Editor from './Editor'
 
-// const lol: number = "WOOT"
-
-export default function Main() {
+export default function Main(): JSX.Element {
   return (
     <>
-
-          <h1>Good luck!!!</h1>
-          <Editor/>
-          </>
+      <h1>Good luck!!!</h1>
+      <Editor/>
+    </>
   )
 }
